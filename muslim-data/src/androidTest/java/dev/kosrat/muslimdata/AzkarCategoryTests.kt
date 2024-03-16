c905fee8345acb08e8395d015511f2cca575e1b1
Kosrat D. Ahmed <kosrat.d.ahmad@gmail.com>
2024-03-16 23:57:06 +0300

    Refactor azkar category tests to use repository instead of Dao

--- muslim-data/src/androidTest/java/dev/kosrat/muslimdata/AzkarCategoryTests.kt
+++ muslim-data/src/androidTest/java/dev/kosrat/muslimdata/AzkarCategoryTests.kt
@@ -1,12 +1,11 @@
 package dev.kosrat.muslimdata
 
 import android.content.Context
-import androidx.room.Room
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import dev.kosrat.muslimdata.database.MuslimDataDao
-import dev.kosrat.muslimdata.database.MuslimDataDatabase
 import dev.kosrat.muslimdata.models.Language
+import dev.kosrat.muslimdata.repository.MuslimRepository
+import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Assert
 import org.junit.Before
@@ -17,28 +16,14 @@ import org.junit.runner.RunWith
 class AzkarCategoryTests {
 
     private lateinit var context: Context
-    private lateinit var muslimDataDatabase: MuslimDataDatabase
-    private lateinit var muslimDataDao: MuslimDataDao
 
     @Before
     fun setup() {
         context = InstrumentationRegistry.getInstrumentation().targetContext
-        muslimDataDatabase = Room.databaseBuilder(
-            context.applicationContext,
-            MuslimDataDatabase::class.java,
-            "muslim_db.db"
-        )
-            .createFromAsset("database/muslim_db_v2.0.0.db")
-            .fallbackToDestructiveMigration()
-            .allowMainThreadQueries()
-            .build()
-
-        muslimDataDao = muslimDataDatabase.muslimDataDao
     }
 
     @After
     fun teardown() {
-        muslimDataDatabase.close()
     }
 
     @Test
@@ -66,9 +51,8 @@ class AzkarCategoryTests {
         testAzkarCategories(Language.RU)
     }
 
-    private fun testAzkarCategories(
-        language: Language
-    ) = muslimDataDao.getAzkarCategories(language = language.value).let { categories ->
+    private fun testAzkarCategories(language: Language) = runBlocking {
+        val categories = MuslimRepository(context).getAzkarCategories(language)
         Assert.assertNotNull(categories)
         Assert.assertEquals(categories.size, 11)
         Assert.assertNotNull(categories[(0..10).random()].categoryName)
